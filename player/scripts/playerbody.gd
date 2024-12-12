d67cc63143b4b107440a3e988003c5592aa40790
SebastianS-HTL <hexatex.sesh@gmail.com>
2024-12-12 10:44:36 +0100

    add the whole (a little clunky) slide and groundpound mechanic

--- player/scripts/playerbody.gd
+++ player/scripts/playerbody.gd
@@ -3,11 +3,15 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var sensitivity = 0 #editable from outside
+var restrictedMovement = Vector3(0,0,0)
+const groundPoundSpeed = -20
 
 var can_slide = true
+var can_crouch = true
 var can_gp = true
 
 var crouched = false
+var sliding = false
 var groundPounding = false
 
 @onready var camera = $Camera3D
@@ -36,8 +40,36 @@ func handle_mouse_look():
 
 	# Reset mouse_delta to avoid repeating the motion
 	mouse_delta = Vector2.ZERO
-
+	
 @export var crouchSpeed = 1
+
+func slide():
+	crouched = true
+	if restrictedMovement != Vector3(velocity.x,0,velocity.z):
+		restrictedMovement = Vector3(velocity.x,0,velocity.z)
+	
+	if Input.is_action_just_released("ctrl"):
+		restrictedMovement = Vector3(0,0,0)
+		sliding = false
+		crouched = false
+
+func groundPound():
+	crouched = true
+	if restrictedMovement != Vector3(0,groundPoundSpeed,0):
+		restrictedMovement = Vector3(0,groundPoundSpeed,0)
+	
+	if is_on_floor():
+		if Input.is_action_pressed("ctrl"):
+			if not Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_down"):
+				crouched = true
+			else:
+				print("dbug")
+				sliding = true
+		else:
+			crouched = false
+		
+		restrictedMovement = Vector3(0,0,0)
+		groundPounding = false
 
 func crouch(delta): #transitioning between crouched and uncrouched
 	delta *= crouchSpeed
@@ -68,13 +100,30 @@ func crouch(delta): #transitioning between crouched and uncrouched
 	get_child(0).position.y = get_child(0).get_scale().y
 
 func _physics_process(delta): # "main"
-	#Engine.max_fps = 30
+	#Engine.max_fps = 30 # in case you think thats needed
 	
 	# check for crouch
 	if Input.is_action_just_pressed("ctrl"):
-		crouched = true
-	if Input.is_action_just_released("ctrl"):
+		if is_on_floor():
+			if velocity == Vector3(0,velocity.y,0):
+				if can_crouch:
+					crouched = true
+			else:
+				sliding = true
+		else:
+			if can_gp:
+				groundPounding = true
+	
+	if (Input.is_action_just_released("ctrl") and not groundPounding) or not can_crouch:
 		crouched = false
+	
+	# trigger slide when needed
+	if sliding:
+		slide()
+	
+	# trigger groundpound when needed
+	if groundPounding:
+		groundPound()
 	
 	# trigger crouch function
 	crouch(delta)
@@ -99,7 +148,12 @@ func _physics_process(delta): # "main"
 		# Stop instantly when no input is given
 		velocity.x = 0
 		velocity.z = 0
-
+	
+	if restrictedMovement != Vector3(0,0,0):
+		velocity.x = 0
+		velocity.z = 0
+		velocity += restrictedMovement # need to keep gravity
+	
 	move_and_slide()
 
 func _on_deathborder_body_shape_entered(body_rid, body, body_shape_index, local_shape_index): #reset position (respawn) when hitting a death barrier
