9baae07d39ae0b87188e124252f7b140d4144af6
SebastianS-HTL <hexatex.sesh@gmail.com>
2024-12-13 10:02:03 +0100

    godot plush

--- pause menu/scripts/fov_slider.gd
+++ pause menu/scripts/fov_slider.gd
@@ -2,8 +2,10 @@ extends HSlider
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var output = get_parent().get_child(4)
+	var output = get_parent().get_child(7)
 	var player = get_parent().get_parent().get_parent().get_child(0)
 	var camera = player.get_child(1)
 	output.set_text(str(value))
-	player.sensitivity = value
+	print(value)
+	print(camera)
+	camera.set_fov(value)
